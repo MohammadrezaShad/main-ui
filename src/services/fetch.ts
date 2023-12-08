5bbfc7d3dea141acf86367def684c713747f9ecd
Javad Hosseinzadeh <javad.hr@gmail.com>
2023-12-08 17:29:38 +0330

    feat: configure graphql

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -1,8 +1,19 @@
-export const gqlFetch = <T>(url: string, query: string, variables?: T) =>
+export const gqlFetch = <T>({
+  url,
+  query,
+  variables,
+  headers,
+}: {
+  url: string;
+  query: string;
+  variables?: T;
+  headers?: HeadersInit;
+}) =>
   fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      ...(headers || {}),
     },
     body: JSON.stringify({
       query,
