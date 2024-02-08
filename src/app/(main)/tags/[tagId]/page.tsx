1bbebc7af0875986715d4790bae99d362c43e0ba
Javad Hosseinzadeh <javad.hr@gmail.com>
2024-02-08 17:19:11 +0330

    fix: display correct title on tag page

--- src/app/(main)/tags/[tagId]/page.tsx
+++ src/app/(main)/tags/[tagId]/page.tsx
@@ -1,19 +1,35 @@
 import {TagsView} from '@/components';
 import {css} from '@styled/css';
+import {dehydrate} from '@tanstack/react-query';
 
-const Page = async ({params}: {params: {tagId: string}}) => (
-  <div
-    className={css({
-      display: 'flex',
-      flexDir: 'column',
-      rowGap: 8,
-      mx: 'auto',
-      maxWidth: '960px',
-      p: {lgDown: 4},
-    })}
-  >
-    <TagsView />
-  </div>
-);
+import {FindTagBySlug} from '@/graphql/query/tags';
+import {getQueryClient} from '@/helpers';
+import {Hydrate} from '@/providers';
+
+const Page = async ({params}: {params: {tagId: string}}) => {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['find-tag', params.tagId],
+    queryFn: () => FindTagBySlug({slug: params.tagId}),
+  });
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <div
+      className={css({
+        display: 'flex',
+        flexDir: 'column',
+        rowGap: 8,
+        mx: 'auto',
+        maxWidth: '960px',
+        p: {lgDown: 4},
+      })}
+    >
+      <Hydrate state={dehydratedState}>
+        <TagsView />
+      </Hydrate>
+    </div>
+  );
+};
 
 export default Page;
---
+++ src/graphql/query/tags/index.ts
@@ -0,0 +1,1 @@
+export * from './find-tag-by-slug';
