42994c00ff0d46303aa057cd14f666b2c5220325
Javad Hosseinzadeh <javad.hr@gmail.com>
2024-02-22 18:10:25 +0330

    feat: integrate google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
-import {ToastContainer} from 'react-toastify';
 import type {Metadata} from 'next';
 import {Ubuntu} from 'next/font/google';
 import {cookies} from 'next/headers';
+import {ToastContainer} from 'react-toastify';
 
 import {CookieName, ThemeType} from '@/constants';
 import {MainProviders} from '@/providers';
 
 import '@/styles/globals.css';
+import Script from 'next/script';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -35,6 +36,20 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 
   return (
     <html lang='en' dir='ltr' data-color-mode={theme?.value}>
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTM_ID}`}
+        ></Script>
+        <Script id='google-analytics'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PC0J1ZGFDM');
+          `}
+        </Script>
+      </head>
       <body className={ubuntu.className}>
         <MainProviders theme={theme?.value as ThemeType}>
           <main>
