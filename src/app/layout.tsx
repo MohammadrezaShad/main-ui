b0666d3c8c7cf625f81b97a77f79001b525b8ca6
Javad Hosseinzadeh <javad.hr@gmail.com>
2024-02-22 18:29:55 +0330

    feat: integrate google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,9 +38,16 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang='en' dir='ltr' data-color-mode={theme?.value}>
       <head>
+        <Script id='google-tag-manager'>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER_API_KEY}');`}
+        </Script>
         <Script
           async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTM_ID}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_API_KEY}`}
         ></Script>
         <Script id='google-analytics'>
           {`
@@ -52,6 +59,14 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         </Script>
       </head>
       <body className={ubuntu.className}>
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-5BDQMKVV'
+            height='0'
+            width='0'
+            style={{display: 'none', visibility: 'hidden'}}
+          ></iframe>
+        </noscript>
         <MainProviders theme={theme?.value as ThemeType}>
           <main>
             <div className='root'>
