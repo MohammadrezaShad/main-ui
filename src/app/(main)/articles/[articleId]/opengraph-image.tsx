73274d8d9400a5c4398a9bded6f9ce2ab78a6fe3
Javad Hosseinzadeh <javad.hr@gmail.com>
2024-03-29 20:25:24 +0330

    fix: display article's og image in proper size and format

--- src/app/(main)/articles/[articleId]/opengraph-image.tsx
+++ src/app/(main)/articles/[articleId]/opengraph-image.tsx
@@ -9,8 +9,8 @@ import {ArticleType} from '@/graphql/generated/types';
 const IMAGE_STORAGE_URL = process.env.NEXT_PUBLIC_IMAGE_STORAGE_URL;
 
 export const size = {
-  width: 900,
-  height: 450,
+  width: 1200,
+  height: 630,
 };
 
 export const contentType = 'image/png';
@@ -24,18 +24,18 @@ export default async function Image({params}: {params: {articleId: string}}) {
     (
       <div tw='relative flex items-center justify-center'>
         <img
-          width='900'
-          height='450'
-          src={`${IMAGE_STORAGE_URL}/${post.thumbnail?._id}`}
+          width='1200'
+          height='630'
+          src={`${IMAGE_STORAGE_URL}/${post.thumbnail?._id}?w=1200&h=630`}
           alt={post.title ?? ''}
           style={{
             objectFit: 'cover',
           }}
         />
         <div tw='absolute bg-black flex opacity-0.5 inset-0' />
-        <div tw='absolute top-2 flex items-center w-full'>
-          <p tw='text-gray-700 text-center text-4xl font-bold m-5'>{post.title}</p>
-          <p tw='text-gray-500 text-xl font-bold m-5'>{post.author.displayName}</p>
+        <div tw='absolute top-2 left-2 flex flex-col bg-white/65 w-max rounded-2xl'>
+          <p tw='text-gray-700 text-center text-4xl font-bold mt-5 mx-5 capitalize'>{post.title}</p>
+          <p tw='text-gray-500 text-xl font-bold mx-5'>By:&nbsp;{post.author.displayName}</p>
         </div>
       </div>
     ),
