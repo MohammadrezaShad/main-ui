e2f44646d1eaa1e86a1edd2fc3e6674593311eec
Javad Hosseinzadeh <javad.hr@gmail.com>
2024-06-15 11:51:51 +0330

    feat: set basic metadata for author page

--- src/app/(main)/author/[authorId]/page.tsx
+++ src/app/(main)/author/[authorId]/page.tsx
@@ -1,12 +1,50 @@
 import {css} from '@styled/css';
 import {dehydrate} from '@tanstack/react-query';
+import {Metadata} from 'next';
 import {cookies} from 'next/headers';
 
 import {AuthorView} from '@/components';
 import {CookieName} from '@/constants';
-import {findUserById, searchArticlesByAuthorId} from '@/graphql';
+import {findUserById, searchArticlesByAuthorId, User} from '@/graphql';
 import {getQueryClient} from '@/helpers';
 import {Hydrate} from '@/providers';
+
+export async function generateMetadata({params}: {params: {authorId: string}}): Promise<Metadata> {
+  const cookieStore = cookies();
+  const authToken = cookieStore.get(CookieName.AUTH_TOKEN)?.value || '';
+  const data: any = await findUserById({id: params.authorId}, authToken);
+  if (!data) {
+    return {
+      title: 'Not found',
+      description: 'The page not found',
+    };
+  }
+  const author: User = data.users!.findUserById;
+
+  function getAuthorName() {
+    if (!author) return 'Unknow Author';
+    if (author.displayName) return author.displayName;
+    if (author.firstName && author.lastName) return `${author.firstName} ${author.lastName}`;
+    if (author.username) return author.username;
+    return 'Unknow Author';
+  }
+
+  return {
+    title: getAuthorName(),
+    description: '',
+    alternates: {
+      canonical: `${process.env.NEXT_PUBLIC_BASE_URL}/author/${params.authorId}`,
+    },
+    robots: {
+      follow: true,
+      index: true,
+      googleBot: {
+        follow: true,
+        index: true,
+      },
+    },
+  };
+}
 
 const Page = async ({params}: {params: {authorId: string}}) => {
   const cookieStore = cookies();
