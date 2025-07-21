6bd1ed32c03bbb42488821053a4b4228dbb43a50
shadmobaraki <m.shadmobaraki@gmail.com>
2025-07-21 09:13:23 +0330

    feat : add redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,15 @@ import {NextResponse} from 'next/server';
 import {v4 as uuidv4} from 'uuid';
 
 import {CookieName} from '@/constants/coooki-name.enum';
+import {findRedirectByPath} from '@/graphql/query/redirect/find-redirect-by-path';
+
+enum RedirectStatusEnum {
+  PERMANENT_TRANSFER = 301,
+  DETECTED = 302,
+  SEE_OTHER = 303,
+  TEMPORARY_VEER = 307,
+  PERMANENT_REDIRECT = 308,
+}
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
@@ -12,6 +21,16 @@ export async function middleware(req: NextRequest) {
   const originalPath = req.headers.get('x-original-url') || new URL(req.url).pathname;
   const profilePath = '/profile';
   const homePath = '/';
+
+  const redirectData = await findRedirectByPath({oldPath: decodeURIComponent(originalPath)});
+
+  if (redirectData?.result?.newPath) {
+    return NextResponse.redirect(
+      new URL(redirectData.result.newPath, req.url),
+      RedirectStatusEnum[redirectData.result.type as keyof typeof RedirectStatusEnum] ||
+        RedirectStatusEnum.PERMANENT_TRANSFER,
+    );
+  }
 
   if (!clientId) {
     setCookie(CookieName.CLIENT_ID, uuidv4(), {
